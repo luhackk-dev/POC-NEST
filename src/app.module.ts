a922e9d4193c80cdd7b6a5a31ea11f906c0d1d79
Lucas Seganzerla Macedo <lsmacedo.code@gmail.com>
2026-01-21 14:43:42 -0300

    Add User Module on App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ProductsModule],
