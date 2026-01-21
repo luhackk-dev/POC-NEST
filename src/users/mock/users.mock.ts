32f64f42d7a11b5eb092190a646a2e2aa2625bbf
Lucas Seganzerla Macedo <lsmacedo.code@gmail.com>
2026-01-21 11:40:19 -0300

    Created  a mock of two users

---
+++ src/users/mock/users.mock.ts
@@ -0,0 +1,14 @@
+import { User } from "../entities/user.entity"
+
+export const usersMock: User[] = [
+  {
+    id: 1,
+    name: 'Jonas',
+    email: 'Jonas@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Ricardinho',
+    email: 'Ricardinho@gmail.com'
+  }
+]
