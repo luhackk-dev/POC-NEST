2b2767ad6dd288c2ab61fbb5663ef63a20f67a52
luhackk <luhack@gmail.com>
2026-01-29 18:12:48 -0300

    add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,11 @@
 export class User {
-  id: number;
+  id: string;
   name: string;
   email: string;
+
+  public constructor(id: string, name: string, email: string){
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
 }
