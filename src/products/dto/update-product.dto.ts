ad4f2f3ab840bbba4dea4e37758547c20ecacfae
Lucas <lsmacedo.code@gmail.com>
2026-02-04 17:33:44 -0300

    add document update-product-dto

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,6 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class UpdateProductDto {
+  
+  @ApiProperty({example: "Notebook"})
   name?: string;
+
+  @ApiProperty({example: "versatile tool for writing, note-taking, and organizing thoughts"})
   description?: string;
+
+  @ApiProperty ({example: 2500})
   price?: number;
+  
+  @ApiProperty ({example: 10})
   quantity?: number;
 }
