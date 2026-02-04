b075e734574ecaedf86a774a508e9faff042bc22
Lucas <lsmacedo.code@gmail.com>
2026-02-04 17:40:00 -0300

    add document update-user-dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class UpdateUserDto {
+
+  @ApiProperty({example: "João"})
   name?: string;
+
+  @ApiProperty({example: "joaoemail@gmail.com"})
   email?: string;
 }
