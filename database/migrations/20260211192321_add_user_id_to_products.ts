9ff7217920319fb9d6c0049133295bc4dffa08da
Lucas <lsmacedo.code@gmail.com>
2026-02-11 16:30:47 -0300

    add migration

---
+++ database/migrations/20260211192321_add_user_id_to_products.ts
@@ -0,0 +1,23 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.table('products', (table) => {
+        table.integer('user_id')
+        .unsigned().
+        references('id').
+        inTable('users').
+        onDelete('CASCADE');
+
+        table.index('user_id');
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.table('products', (table) => {
+        table.dropIndex('user_id');
+        table.dropColumn('user_id');
+    })
+}
+
